a90a80595b45c60aea544b2d504229bbf092666b
TomNewChao <353712216@qq.com>
2022-07-11 14:54:26 +0800

    测试make_rpm.sh完成

--- metrics/node_exporter/make_rpm.sh
+++ metrics/node_exporter/make_rpm.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cur_path=$(pwd)
 top_dir=$(rpm --eval "%{_topdir}")
-cd cur_path"/../"
+cd $cur_path"/../"
 tar -zcvf node_exporter-1.3.1-0.tar.gz node_exporter
-cp node_exporter-1.3.1-0.tar.gz top_dir"/SOURCES"
-make
+cp node_exporter-1.3.1-0.tar.gz $top_dir"/SOURCES"
+cd $cur_path
 rpmbuild -ba node_exporter.spec
 
