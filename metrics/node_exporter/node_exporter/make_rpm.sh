670f11ac22172b26d7bc56ed70b8670d3330cd16
TomNewChao <353712216@qq.com>
2022-09-06 12:26:02 +0800

    0906增加arm包

--- metrics/node_exporter/node_exporter/make_rpm.sh
+++ metrics/node_exporter/node_exporter/make_rpm.sh
@@ -8,7 +8,12 @@ top_dir=$(rpm --eval "%{_topdir}")
 cd $cur_path
 # arm: CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC_FOR_TARGET=gcc-aarch64-linux-gnu CC=aarch64-linux-gnu-gcc go build
 go build node_exporter.go
-pkg_name="/node_exporter-"$version"-0.x86_64"
+is_x86=`(uname -i)`
+if [[ $is_x86 == "x86_64" ]];then
+  pkg_name="/node_exporter-"$version"-0.x86_64"
+else
+  pkg_name="/node_exporter-"$version"-0.aarch64"
+fi
 build_root_path="/BUILDROOT"
 rm -rf $top_dir$build_root_path$pkg_name
 mkdir -p $top_dir$build_root_path$pkg_name"/usr/bin"
